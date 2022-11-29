1cc1e2de19e8c8d92ae25c837f3c2001c7ddb454
Marcel Noe <marcel@marcel-noe.de>
2022-11-29 21:42:15 +0100

    added more local directory snapshots for tape backup

--- tapebackup.sh
+++ tapebackup.sh
@@ -6,8 +6,23 @@ echo "Configuration: $1"
 zfs destroy vault/encrypted@amanda_$1
 zfs snapshot vault/encrypted@amanda_$1
 
+zfs destroy vault/backup@amanda_$1
+zfs snapshot vault/backup@amanda_$1
+
+btrfs subvol delete /snapshot_amanda_$1
+btrfs subvol snapshot -r / /snapshot_amanda_$1
+
+btrfs subvol delete /home/snapshot_amanda_$1
+btrfs subvol snapshot -r /home /home/snapshot_amanda_$1
+
+btrfs subvol delete /var/lib/libvirt/images/snapshot_amanda_$1
+btrfs subvol snapshot -r /var/lib/libvirt/images /var/lib/libvirt/images/snapshot_amanda_$1
+
 # Trigger amanda
 ssh root@10.10.32.4 "./dump.sh $1"
 
 # Delete snapshot
 zfs destroy vault/encrypted@amanda_$1
+zfs destroy vault/backup@amanda_$1
+btrfs subvol delete /snapshot_amanda_$1
+
