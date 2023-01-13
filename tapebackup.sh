5fc52cb30ce121496812faf30c6c170ebda68184
Marcel Noe <marcel@marcel-noe.de>
2023-01-13 23:56:27 +0100

    changed backup from miranda to localhost

--- tapebackup.sh
+++ tapebackup.sh
@@ -18,16 +18,23 @@ btrfs subvol snapshot -r /home /home/snapshot_amanda_$1
 btrfs subvol delete /var/lib/libvirt/images/snapshot_amanda_$1
 btrfs subvol snapshot -r /var/lib/libvirt/images /var/lib/libvirt/images/snapshot_amanda_$1
 
+echo "Created all snapshots"
+
 # Show next tapes to use
-ssh backup@miranda.lan.ka.xcore.net "/usr/sbin/amadmin $1 tape"
+#ssh backup@miranda "/usr/sbin/amadmin $1 tape"
+su backup -c "/usr/sbin/amadmin $1 tape"
 echo "Please stage tapes and press Enter"
 read
 
 # Trigger amanda
-ssh backup@miranda.lan.ka.xcore.net "/usr/sbin/amcheck $1"
+#ssh backup@miranda "/usr/sbin/amcheck $1"
+su backup -c "/usr/sbin/amcheck $1"
+
+echo "Check done, proceeding with dump"
 #echo "Press Enter"
 #read
-ssh backup@miranda.lan.ka.xcore.net "/usr/sbin/amdump $1"
+#ssh backup@miranda "/usr/sbin/amdump $1"
+su backup -c "/usr/sbin/amdump $1"
 
 # Delete snapshot
 zfs destroy vault/encrypted@amanda_$1
