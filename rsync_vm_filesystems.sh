56c01d431eb2c63c0c00b9fc497e9246ef30494d
Marcel Noe <marcel@marcel-noe.de>
2023-08-06 09:40:28 +0200

    added delete option, especially for changed symlinks

--- rsync_vm_filesystems.sh
+++ rsync_vm_filesystems.sh
@@ -8,13 +8,13 @@ export PATH=/opt/emulex/ocmanager/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/
 # Copy data of VM filesystems that should also be backed up separately
 #
 ### Backup wormhole01 
-rsync -prav root@wormhole01.xcore.net:/ /vault/encrypted/crypto_backup/vms/wormhole01/ --exclude /proc --exclude /sys
+rsync -prav root@wormhole01.xcore.net:/ /vault/encrypted/crypto_backup/vms/wormhole01/ --exclude /proc --exclude /sys --delete
 
 ### Backup ns2 
-rsync -prav root@ns2.noetech.net:/ /vault/encrypted/crypto_backup/vms/ns2/ --exclude /proc --exclude /sys
+rsync -prav root@ns2.noetech.net:/ /vault/encrypted/crypto_backup/vms/ns2/ --exclude /proc --exclude /sys --delete
 
 ### Backup dockerserver
-rsync -prav root@dockerserver.services.ka.xcore.net:/opt/ /vault/encrypted/crypto_backup/vms/dockerserver/opt/
+rsync -prav root@dockerserver.services.ka.xcore.net:/opt/ /vault/encrypted/crypto_backup/vms/dockerserver/opt/ --delete
 
 ## Backup archive
-rsync -prav root@archive.services.ka.xcore.net:/ /vault/encrypted/crypto_backup/vms/archive/ --exclude /proc --exclude /sys
+rsync -prav root@archive.services.ka.xcore.net:/ /vault/encrypted/crypto_backup/vms/archive/ --exclude /proc --exclude /sys --delete
